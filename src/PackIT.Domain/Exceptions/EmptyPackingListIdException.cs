2cfa76bfcd138decf1289b5d12d8dbaa8faf6de7
Dong Zeng <dong@freshbyte.com>
2021-11-22 09:09:13 -0600

    converted PackingListId as value object

---
+++ src/PackIT.Domain/Exceptions/EmptyPackingListIdException.cs
@@ -0,0 +1,11 @@
+﻿using PackIT.Shared.Exceptions;
+
+namespace PackIT.Domain.Exceptions
+{
+    public class EmptyPackingListIdException : PackItException
+    {
+        public EmptyPackingListIdException() : base("Packing list ID cannot be empty.")
+        {
+        }
+    }
+}

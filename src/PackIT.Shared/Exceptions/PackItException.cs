0bb80119cad69dd9dd320def5fc8cb0c25a15e60
Dong Zeng <zeng-dong@hotmail.com>
2023-01-03 15:41:56 -0600

    exception to conform to iserializable pattern

--- src/PackIT.Shared/Exceptions/PackItException.cs
+++ src/PackIT.Shared/Exceptions/PackItException.cs
@@ -1,10 +1,22 @@
-﻿namespace PackIT.Shared.Exceptions
+﻿using System.Runtime.Serialization;
+
+namespace PackIT.Shared.Exceptions
 {
+    [Serializable]
     public abstract class PackItException : Exception
     {
         protected PackItException(string message) : base(message)
         {
+        }
 
+        protected PackItException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+        {
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
         }
     }
 }
